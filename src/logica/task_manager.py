29f6e54777c7286216449d678091a123f8d49888
Ernesto <74765942@continental.edu.pe>
2026-02-09 02:02:14 -0500

    test verde 2: HU-001 crear y seleccionar perfil

--- src/logica/task_manager.py
+++ src/logica/task_manager.py
@@ -1,3 +1,18 @@
+from src.modelo.declarative_base import Session
+from src.modelo.modelo import Perfil
+
 class TaskManager:
+    def crear_perfil(self, nombre):
+        session = Session()
+        perfil = Perfil(nombre=nombre)
+        session.add(perfil)
+        session.commit()
+        session.refresh(perfil)
+        session.close()
+        return perfil
+
     def seleccionar_perfil(self, id_perfil):
-        return None
+        session = Session()
+        perfil = session.query(Perfil).filter_by(idPerfil=id_perfil).first()
+        session.close()
+        return perfil
