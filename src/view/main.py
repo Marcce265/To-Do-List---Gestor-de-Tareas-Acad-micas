700044bdcc3b253a4ae36df85c11734c046749a9
Marcce265 <74765942@continental.edu.pe>
2026-02-19 00:46:44 -0500

    avanze hasta aqui

--- src/view/main.py
+++
@@ -1,9 +0,0 @@
-import flet as ft
-
-def main(page: ft.Page):
-    page.title = "To-Do List Académico"
-    page.add(
-        ft.Text("Hola Ernesto, Flet está funcionando 🚀")
-    )
-
-ft.run(main)
---
+++ src/view/screens/__init__.py
@@ -0,0 +1,9 @@
+from src.view.screens.login_screen import LoginScreen
+from src.view.screens.main_screen import MainScreen
+from src.view.screens.materia_screen import MateriaScreen
+
+__all__ = [
+    'LoginScreen',
+    'MainScreen',
+    'MateriaScreen',
+]
