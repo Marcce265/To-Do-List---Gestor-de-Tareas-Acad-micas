d8d5f34893bde5bfdda32e8ada928c7b93b568dc
Marcce265 <74765942@continental.edu.pe>
2026-02-19 02:31:27 -0500

    avanze de ui-usuarios

--- src/view/screens/__init__.py
+++
@@ -1,9 +0,0 @@
-from src.view.screens.login_screen import LoginScreen
-from src.view.screens.main_screen import MainScreen
-from src.view.screens.materia_screen import MateriaScreen
-
-__all__ = [
-    'LoginScreen',
-    'MainScreen',
-    'MateriaScreen',
-]
